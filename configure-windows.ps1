adbd4fd00b2e581eebc1119a001d66e4268d9516
Alex Kochnev <akochnev@apprenda.com>
2018-05-08 15:39:33 -0400

    Tweaked configure-windows.ps1 with the correct example

--- configure-windows.ps1
+++ configure-windows.ps1
@@ -6,9 +6,7 @@
     https://github.com/bsteciuk/k8s-ovn-windows-cluster
     https://kubernetes.io/docs/getting-started-guides/windows/
 .EXAMPLE  
-   > .\ovnkube.exe -k8s-apiserver https://172.20.2.30:6443  -k8s-cacert "C:\etc\kubernetes\pki\ca.crt" -init-node "win-gd0rkodikbq" -nb-address "tcp://172.20.2.30:6641" -sb-address "tcp://172.20.2.30:6642" -cluster-subnet "192.168.0.0/16" -cni-conf-dir "C:\cni-conf" -k8s-token "<token from kubeadm>"
-   > INFO[0000] Parsed config file C:\Program Files\Cloudbase Solutions\Open vSwitch\conf\\ovn_k8s.conf
-   > INFO[0000] Parsed config: {Default:{MTU:1500 ConntrackZone:64321} Logging:{File:C:\var\log\ovnkube.log Level:5} CNI:{ConfDir:C:\cni-conf Plugin:ovn-k8s-cni-overlay WinHNSNetworkID:} Kubernetes:{Kubeconfig: CACert:C:\etc\kubernetes\pki\ca.crt APIServer: Token:} OvnNorth:{Address: ClientPrivKey: ClientCert: ClientCACert: ServerPrivKey: ServerCert: ServerCACert:} OvnSouth:{Address: ClientPrivKey: ClientCert: ClientCACert: ServerPrivKey: ServerCert: ServerCACert:}}
+   .\configure-windows.ps1 -gatewayIp 192.168.3.1 -ovnHostSubnet 192.168.3.0/24 -networkInterfaceAlias "Ethernet 2"
 .FUNCTIONALITY  
    Not sure How to specify or use  
    Does not appear in basic, -full, or -detailed  
